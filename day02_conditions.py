fe99b7770afcfbe4266ec9495c2daca7b6262f0d
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-01-23 15:07:52 +0530

    Create day02_variablesoperators

---
+++ day02_conditions.py
@@ -0,0 +1,15 @@
+#------------------------------------
+#day2_variablesoperators
+#------------------------------------
+
+#1.Even or Odd
+
+num=int(input("Enter the number :"))
+if(num%2==0):
+    print("The given number is even")
+else:
+    print("The given number is odd")
+print("-------------------------------")
+
+
+#2.
