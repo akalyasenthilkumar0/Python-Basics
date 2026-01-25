93c466cbf395c7d558c52c9b9ded3b6c97e581bb
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-01-25 10:51:49 +0530

    Create day_3conditionals.py

---
+++ day_3conditionals.py
@@ -0,0 +1,8 @@
+#--------------------------------------
+#day3_conditionals
+#--------------------------------------
+
+#1.Grade calculator 
+
+name=str(input("Enter name:"))
+marks=int(input("Enter marks:\nEnglish=\nMaths=\nScience="))
