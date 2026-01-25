c4be6e56b0415df81e9e5a023070e1fbc9c488d4
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-01-25 11:11:05 +0530

    Update day_3conditionals.py

--- day_3conditionals.py
+++ day_3conditionals.py
@@ -2,7 +2,25 @@
 #day3_conditionals
 #--------------------------------------
 
+
 #1.Grade calculator 
 
 name=str(input("Enter name:"))
-marks=int(input("Enter marks:\nEnglish=\nMaths=\nScience="))
+English=int(input("Enter marks in English="))
+Maths=int(input("Enter marks in Maths="))
+Science=int(input("Enter marks in Science="))
+avg=(English+Maths+Science)//3
+print("Average Marks=",avg)
+print("Calculating the Grade:")
+if (avg>85):
+    print(" Grade:'A' ")
+elif (avg>65 and avg<=85):
+    print("Grade:'B' ")
+elif (avg>45 and avg<=65):
+    print("Garde:'C' ")
+else:
+    print("Failed!")
+print("----------------------------------")
+
+
+#2.
